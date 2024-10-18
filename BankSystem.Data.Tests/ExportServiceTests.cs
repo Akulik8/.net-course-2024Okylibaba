c741332e71e60b6b5fd1a854061c2a7c03d9488c
Akulik <Akulik03@icloud.com>
2024-10-19 01:22:49 +0300

    FileStream feat: Реализовано взаимодействие с CSV файлами

---
+++ BankSystem.Data.Tests/ExportServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using BankSystem.App.Interfaces;
+using BankSystem.App.Services;
+using BankSystem.Data.Storages;
+using ExportTool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.Data.Tests
+{
+    public class ExportServiceTests
+    {
+        [Fact]
+        public void WriteClientsToCsvPositiveTest()
+        {
+            //Arrange
+            IClientStorage storage = new ClientStorage(new BankSystemDbContext());
+            var clients = storage.Get(10,1,null);
+            var exportService = new ExportService(@"E:\Practic\.net-course-2024Okylibaba\", "WriteClients.csv");
+
+            //Act
+            exportService.WriteClientsToCsv(clients);
+            var readClients = exportService.ReadClientsFromCsv();
+
+            //Asssert
+            Assert.Equal(clients.Count, readClients.Count);
+        }
+
+        [Fact]
+        public void ReadClientsFromCsvPositiveTest()
+        {
+            //Arrange
+            IClientStorage storage = new ClientStorage(new BankSystemDbContext());
+            var testDataGenerator = new TestDataGenerator();
+            var clients = testDataGenerator.GenerateClients(8);
+            var exportService = new ExportService(@"E:\Practic\.net-course-2024Okylibaba\", "ReadClients.csv");
+
+            //Act
+            exportService.WriteClientsToCsv(clients);
+            var readClients = exportService.ReadClientsFromCsv();
+
+            foreach (var client in readClients) 
+            {
+                storage.Add(client);
+            }
+
+            //Assert
+            Assert.NotEmpty(readClients);
+        }
+    }
+}
