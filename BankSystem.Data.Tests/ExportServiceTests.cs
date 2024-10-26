c9706f3a0eaf5b5e374abdccf348b921b8fb6c59
Akulik <Akulik03@icloud.com>
2024-10-27 01:07:07 +0300

    Threads fixt: Исправлена опечатка в начислении денег на счет

--- BankSystem.Data.Tests/ExportServiceTests.cs
+++ BankSystem.Data.Tests/ExportServiceTests.cs
@@ -163,25 +163,14 @@ namespace BankSystem.Data.Tests
                 Id = new Guid(),
                 Name = "Gleb",
                 Surname = "Ivanov",
-                PhoneNumber = "44444434",
+                PhoneNumber = "4444442543",
                 Date = new DateOnly(2000, 1, 1),
-                Passport = "44444444444234",
+                Passport = "44444444434544",
                 Address = "-----",
             };
-            Client client2 = new Client
-            {
-                Id = new Guid(),
-                Name = "Gleb",
-                Surname = "Ivanov",
-                PhoneNumber = "44444456",
-                Date = new DateOnly(2000, 1, 1),
-                Passport = "4444444444454",
-                Address = "-----",
-            };
-            List<Client> wsef = new List<Client> { client, client2 };
 
             //Act
-            exportService.WritePersonsToFileJson(wsef, @"E:\Practic\.net-course-2024Okylibaba\", "ReadPersons.json");
+            exportService.WritePersonsToFileJson(client, @"E:\Practic\.net-course-2024Okylibaba\", "ReadPersons.json");
             var readClient = exportService.ReadPersonsFromFileJson<Client>(@"E:\Practic\.net-course-2024Okylibaba\", "ReadPersons.json");
             storage.Add(readClient);
 
