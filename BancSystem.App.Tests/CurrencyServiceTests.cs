41c80ff8ac22efaf1c4913911c6f56cd8415b970
Akulik <Akulik03@icloud.com>
2024-11-05 19:56:29 +0200

    HttpClient feat: реализован сервис конвертации валюты

---
+++ BancSystem.App.Tests/CurrencyServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankSystem.App.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using BankSystem.Domain.Models;
+using BankSystem.App.DTOs;
+using Xunit.Abstractions;
+
+namespace BankSystem.App.Tests
+{
+    public class CurrencyServiceTests
+    {
+        private readonly ITestOutputHelper _testOutput;
+        CurrencyService _currencyService;
+
+        public CurrencyServiceTests(ITestOutputHelper _testOutput)
+        {
+            this._testOutput = _testOutput;
+
+            _currencyService = new CurrencyService(Settings.Default.apiKey, Settings.Default.baseUrl);
+        }
+
+        [Fact]
+        public async Task GetCurrencyInfo()
+        {
+            CurrencyData currencyData = new CurrencyData()
+            {
+                To = "USD",
+                From = "EUR",
+                Amount = 100
+            };
+
+            CurrencyResponse currencyResponse = await _currencyService.GetCurrency(currencyData);
+
+            _testOutput.WriteLine(currencyResponse.Amount.ToString(), OutputLevel.Information);
+        }
+
+    }
+}
---
+++ BankSystem.App/Services/CurrencyService.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using BankSystem.Domain.Models;
+using BankSystem.App.DTOs;
+using System.Runtime.CompilerServices;
+
+namespace BankSystem.App.Services
+{
+    public class CurrencyService
+    {
+        private readonly string _apiKey;
+        private readonly string _baseUrl;
+
+        public CurrencyService(string apiKey, string baseUrl)
+        {
+            _apiKey = apiKey;
+            _baseUrl = baseUrl;
+        }
+
+        public async Task<CurrencyResponse> GetCurrency(CurrencyData data)
+        {
+            UriBuilder uriBuilder = new UriBuilder(_baseUrl);
+
+            var query = System.Web.HttpUtility.ParseQueryString(uriBuilder.Query);
+            query["api_key"] = _apiKey;
+            query["from"] = data.From;
+            query["to"] = data.To;
+            query["amount"] = data.Amount.ToString();
+
+            uriBuilder.Query = query.ToString();
+
+            string finalUrl = uriBuilder.ToString();
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage responseMessage = await client.GetAsync(finalUrl);
+                string message = await responseMessage.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<CurrencyResponse>(message);
+            }
+        }
+    }
+}
